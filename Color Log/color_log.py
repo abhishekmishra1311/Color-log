e6db6e0e6c7da72eee3a170ffd9993baffa306ab
Shiavnski <abhishekmishraa1311@gmail.com>
2026-01-21 17:08:22 +0530

    initial

---
+++ Color Log/color_log.py
@@ -0,0 +1,16 @@
+import logging
+
+import coloredlogs
+
+logger = logging.getLogger('my_logger')
+coloredlogs.install(level='DEBUG', logger=logger)
+
+def main():
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
+    
+if __name__ == '__main__':
+    main()
